1e503b5623179849f965cea8dea9193cc82ee71b
MgKyaw <mgkyaw20112@gmail.com>
2025-10-28 21:43:11 +0700

    feat: Add BenchmarkConsoleDemo

---
+++ BenchmarkingConsoleDemo/BenchmarkLINQPerformance.cs
@@ -0,0 +1,24 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkingConsoleDemo;
+public class BenchmarkLINQPerformance
+{
+    private readonly List<string>
+    data = new List<string>();
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        for (int i = 65; i < 90; i++)
+        {
+            char c = (char)i;
+            data.Add(c.ToString());
+        }
+    }
+
+    [Benchmark]
+    public string Single() => data.SingleOrDefault(x => x.Equals("M"));
+
+    [Benchmark]
+    public string First() => data.FirstOrDefault(x => x.Equals("M"));
+}
