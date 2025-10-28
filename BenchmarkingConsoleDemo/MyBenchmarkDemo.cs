11d67e912e887ac7a9cbb5f9b5f22aa987388865
MgKyaw <mgkyaw20112@gmail.com>
2025-10-28 21:52:39 +0700

    fix: Run benchmark

--- BenchmarkingConsoleDemo/MyBenchmarkDemo.cs
+++ BenchmarkingConsoleDemo/MyBenchmarkDemo.cs
@@ -1,34 +1,31 @@
-﻿using BenchmarkDotNet.Attributes;
-using Microsoft.AspNetCore.Mvc.Testing;
+﻿//[MemoryDiagnoser]
+//public class MyBenchmarkDemo
+//{
+//    private static HttpClient _httpClient;
 
-[MemoryDiagnoser]
-public class MyBenchmarkDemo
-{
-    private static HttpClient _httpClient;
+//    [GlobalSetup]
+//    public void GlobalSetup()
+//    {
+//        //var factory = new WebApplicationFactory<Startup>()
+//        //.WithWebHostBuilder(configuration =>
+//        //{
+//        //    configuration.ConfigureLogging(logging =>
+//        //    {
+//        //        logging.ClearProviders();
+//        //    });
+//        //});
 
-    [GlobalSetup]
-    public void GlobalSetup()
-    {
-        var factory = new WebApplicationFactory<Startup>()
-        .WithWebHostBuilder(configuration =>
-        {
-            configuration.ConfigureLogging(logging =>
-            {
-                logging.ClearProviders();
-            });
-        });
+//        //_httpClient = factory.CreateClient();
+//    }
 
-        _httpClient = factory.CreateClient();
-    }
-
-    [Benchmark]
-    public void MyFirstBenchmarkMethod()
-    {
-        //Write your code here   
-    }
-    [Benchmark]
-    public void MySecondBenchmarkMethod()
-    {
-        //Write your code here
-    }
-}
+//    [Benchmark]
+//    public void MyFirstBenchmarkMethod()
+//    {
+//        //Write your code here   
+//    }
+//    [Benchmark]
+//    public void MySecondBenchmarkMethod()
+//    {
+//        //Write your code here
+//    }
+//}
