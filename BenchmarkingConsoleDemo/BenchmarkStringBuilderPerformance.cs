45a41575ac4fcc1237f4c6b2b6ccfc02e0629957
MgKyaw <mgkyaw20112@gmail.com>
2025-10-28 21:58:42 +0700

    feat: Add BenchmarkStringBuilderPerformance

---
+++ BenchmarkingConsoleDemo/BenchmarkStringBuilderPerformance.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace BenchmarkingConsoleDemo;
+
+[MemoryDiagnoser]
+[Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+public class BenchmarkStringBuilderPerformance
+{
+    const string message = "Some text for testing purposes only.";
+    const int CTR = 10000;
+}
