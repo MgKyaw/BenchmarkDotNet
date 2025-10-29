e41ac4148e0860526a5de4cb6bac124414a8ef65
MgKyaw <mgkyaw20112@gmail.com>
2025-10-29 17:21:06 +0700

    fix: Set Benchmark for BenchmarkAPIPerformance

---
+++ BenchmarkingWebDemo/BenchmarkAPIPerformance.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace BenchmarkingWebDemo;
+
+public class BenchmarkAPIPerformance
+{
+    private static HttpClient _httpClient;
+
+    [GlobalSetup]
+    public void GlobalSetup()
+    {
+        var factory = new WebApplicationFactory
+        <Startup>().WithWebHostBuilder(configuration =>
+        {
+            configuration.ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+            });
+        });
+
+        _httpClient = factory.CreateClient();
+    }
+
+    [Benchmark]
+    public async Task GetResponseTime()
+    {
+        var response = await _httpClient.GetAsync("/");
+    }
+}
+
